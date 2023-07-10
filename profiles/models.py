e6078f19639c289c82ea9d2967761bc9d1142b1a
AlexSmall96 <alex.small739@gmail.com>
2023-07-10 18:24:43 +0000

    "

--- profiles/models.py
+++ profiles/models.py
@@ -8,7 +8,8 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    skills = models.TextField(blank=True)
+    interests = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_k3tfhd.jpg'
     )

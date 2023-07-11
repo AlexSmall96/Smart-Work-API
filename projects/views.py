a909489dd6c7876ee70b2e25c717f0a1ea7ac2df
AlexSmall96 <alex.small739@gmail.com>
2023-07-11 15:43:41 +0000

    "Refactor projects and profiles views with generics"

--- projects/views.py
+++ projects/views.py
@@ -1,73 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from smart_work_api.permissions import IsOwnerOrReadOnly
 from .models import Project
 from .serializers import ProjectSerializer
-from smart_work_api.permissions import IsOwnerOrReadOnly
 
 
-class ProjectList(APIView):
+class ProjectList(generics.ListCreateAPIView):
+    """
+    List projects or create a project if logged in
+    The perform_create method associates the project with the logged in user.
+    """
     serializer_class = ProjectSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Project.objects.all()
 
-    def get(self, request):
-        Projects = Project.objects.all()
-        serializer = ProjectSerializer(
-            Projects, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProjectSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class ProjectDetail(APIView):
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a project and edit or delete it if you own it.
+    """
+    serializer_class = ProjectSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = ProjectSerializer
-
-    def get_object(self, pk):
-        try:
-            project = Project.objects.get(pk=pk)
-            self.check_object_permissions(self.request, project)
-            return project
-        except Project.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        project = self.get_object(pk)
-        serializer = ProjectSerializer(
-            project, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        project = self.get_object(pk)
-        serializer = ProjectSerializer(
-            project, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        project = self.get_object(pk)
-        project.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Project.objects.all()

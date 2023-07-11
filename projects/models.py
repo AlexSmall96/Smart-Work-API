01ed3bb47491ef23c206b08dca73fe5f0894e720
AlexSmall96 <alex.small739@gmail.com>
2023-07-11 12:27:42 +0000

    "Create projects app and projects model"

---
+++ projects/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    """
+    Project model, related to 'owner', i.e. a User instance.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    complexity = models.CharField(
+        max_length=7, 
+        choices=(
+            ('Low', 'LOW'),
+            ('Medium', 'MEDIUM'),
+            ('High', 'HIGH'),
+        ),
+        default='Low'
+    )
+
+
+    class Meta:
+        ordering = ['due_date']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'

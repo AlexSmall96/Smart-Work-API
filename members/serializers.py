709e6452617a7fc073071f747a94b75306d4ee0b
AlexSmall96 <alex.small739@gmail.com>
2023-07-22 20:30:45 +0000

    "Add fields to members serializers.py"

--- members/serializers.py
+++ members/serializers.py
@@ -7,10 +7,16 @@ class MemberSerializer(serializers.ModelSerializer):
     Serializer for the Member model
     Create method handles the unique constraint on 'owner' and 'followed'
     """
-    owner = serializers.ReadOnlyField(source='profile.owner.username')
     member_name = serializers.ReadOnlyField(source='profile.name')
-    project_title = serializers.ReadOnlyField(source='project.title')
-   
+    title = serializers.ReadOnlyField(source='project.title')
+    start_date = serializers.ReadOnlyField(source='project.start_date')
+    due_date = serializers.ReadOnlyField(source='project.due_date')
+    project_owner_image = serializers.ImageField(source='project.owner.profile.image')
+    project_owner_profile_id = serializers.ReadOnlyField(source='project.owner.profile.id')
+    project_owner_name = serializers.ReadOnlyField(source='project.owner.profile.name')
+    project_owner_username = serializers.ReadOnlyField(source='project.owner.username')
+    complexity = serializers.ReadOnlyField(source='project.complexity')
+
     class Meta:
         model = Member
         fields = '__all__'

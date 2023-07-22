6604226a41ab4baecd4563f5cd75db30977b6a11
AlexSmall96 <alex.small739@gmail.com>
2023-07-22 18:07:27 +0000

    "Add profile filter to members views"

--- members/views.py
+++ members/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from smart_work_api.permissions import IsOwnerOrReadOnly
 from .models import Member
 from projects.models import Project
 from .serializers import MemberSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class MemberList(generics.ListCreateAPIView):
     """
@@ -13,4 +13,14 @@ class MemberList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        'profile'
+    ]
 

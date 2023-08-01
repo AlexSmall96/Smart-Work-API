101bd6efd78a210d9743661fe409d383d4ee76db
AlexSmall96 <alex.small739@gmail.com>
2023-08-01 11:47:39 +0000

    "Remove django signals to test project deletion"

--- members/views.py
+++ members/views.py
@@ -5,7 +5,7 @@ from projects.models import Project
 from profiles.models import Profile
 from .serializers import MemberSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.shortcuts import get_object_or_404
 
 class MemberList(generics.ListCreateAPIView):
@@ -28,14 +28,21 @@ class MemberList(generics.ListCreateAPIView):
         'project'
     ]
 
-    def create_member(sender, instance, created, **kwargs):
-        project = get_object_or_404(Project, pk=instance.id)
-        profile = get_object_or_404(Profile, pk=instance.owner.id)
-        if len(Member.objects.filter(profile=profile, project=project)) == 0:
-            member = Member.objects.create(project=project, profile=profile)
-            member.save()
+    # def create_member(sender, instance, created, **kwargs):
+    #     project = get_object_or_404(Project, pk=instance.id)
+    #     profile = get_object_or_404(Profile, pk=instance.owner.id)
+    #     if len(Member.objects.filter(profile=profile, project=project)) == 0:
+    #         member = Member.objects.create(project=project, profile=profile)
+    #         member.save()
+    
+    # def delete_member(sender, instance, created, **kwargs):
+    #     project = get_object_or_404(Project, pk=instance.id)
+    #     profile = get_object_or_404(Profile, pk=instance.owner.id)
+    #     member = Member.objects.get(project=project, profile=profile)
+    #     member.delete()
         
-    post_save.connect(receiver=create_member, sender=Project)
+    # post_save.connect(receiver=create_member, sender=Project)
+    # pre_delete.connect(receiver=delete_member, sender=Project)
 
 
 class MemberDetail(generics.RetrieveDestroyAPIView):

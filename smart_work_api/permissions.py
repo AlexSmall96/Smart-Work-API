107bd89d9e2179114cf2e31bbf0bb3b5a9e83b75
AlexSmall96 <alex.small739@gmail.com>
2023-08-02 14:43:51 +0000

    "Edit member update or delete permissions"

--- smart_work_api/permissions.py
+++ smart_work_api/permissions.py
@@ -8,7 +8,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
 class IsOwnerOrReadOnlyMember(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return (obj.project.owner == request.user
-            or obj.profile.owner == request.user)
+        # if request.method in permissions.SAFE_METHODS:
+        return True
+        # return (obj.project.owner == request.user
+        #     or obj.profile.owner == request.user)
